f4016c5fc12471de1c2922f8401abb8b2227630e
Jay Pan <sfffaaa@gmail.com>
2020-01-30 12:24:38 +0100

    Add the test case for get next token

--- test.cpp
+++ test.cpp
@@ -21,6 +21,26 @@ TEST(LexerSuite, lexerSkipWhiteSpace)
 	EXPECT_EQ(l.currentChar, '1') << "The currentChar is not correct";
 }
 
+TEST(LexerSuit, lexerGetNextToken)
+{
+	Lexer l = Lexer("1+2");
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "1")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("PLUS", "+")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "2")) << "The token isn't the same";
+}
+
+TEST(LexerSuit, lexerGetNextTokenMultiple)
+{
+	Lexer l = Lexer(" 1 + 2   / 3     * 4");
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "1")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("PLUS", "+")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "2")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("DIV", "/")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "3")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("MUL", "*")) << "The token isn't the same";
+	EXPECT_EQ(l.getNextToken(), Token("INTEGER", "4")) << "The token isn't the same";
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
